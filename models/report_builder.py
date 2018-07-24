f2a005513f708a4998547c791edd01c9ad1fd0dc
Daniel Stewart <mgdog1@googlemail.com>
2018-07-24 16:31:24 +0100

    updated css

--- models/report_builder.py
+++ models/report_builder.py
@@ -1,15 +1,21 @@
+from bs4 import BeautifulSoup
 import re
 
 class ReportBuilder:
     def __init__(self, output_path):
         self.output_path = output_path
 
-    def generate(self, entries, keywords):
+    def generate(self, entries, keywords, should_highlight):
         html = """
         <html>
-            <head></head>
+            <head>
+                <style>
+                    body {{font-family: "Calibri"; font-size: 11pt;}}
+                    h2   {{font-weight: bold; font-size: 11pt;}}
+                </style>
+            </head>
             <body>
-                {}
+                {body}
             </body>
         </html>
         """
@@ -18,17 +24,20 @@ class ReportBuilder:
             for entry in entries:
                 head_html = str(entry.heading.text) #"<h2>{}</h2>".format(entry.heading)
                 body_html = str(entry.body) #"<p>{}</p>".format(entry.body)
-                link_html = "<div><a href={}>{}</a></div>".format(entry.link, entry.link.split('=')[-1])
-                main_body_html += "<h2>{}</h2>".format(head_html)
+                link_html = "<a href={}>{}</a>".format(entry.link, entry.link.split('=')[-1].strip())
+                main_body_html += "<h2>{}</h2>".format(link_html + head_html)
                 main_body_html += body_html
-                main_body_html += link_html
 
-            html = html.format(main_body_html)
-            html = self.highlight_keywords(html, keywords)
-            output_file.write(html)
+            if should_highlight:
+                main_body_html = self.highlight_keywords(main_body_html, keywords)
+
+            html = html.format(body=main_body_html)
+            soup = BeautifulSoup(html, 'lxml')
+            output_file.write(soup.prettify())
 
     def highlight_keywords(self, html, keywords):
         result = html
         for keyword in keywords:
-             result = re.sub(r'\b{}\b'.format(keyword), '<span style="background-color: papayawhip;">{}</span>'.format(keyword), result, flags=re.I)
+            result = re.sub(r'\b{}\b'.format(keyword),
+                            '<span style="color: red;">{}</span>'.format(keyword), result, flags=re.I)
         return result
--- runner.py
+++ runner.py
@@ -4,10 +4,12 @@ from models.spqm_scraper import SPQMScraper
 from models.report_builder import ReportBuilder
 
 cwd = os.getcwd()
+
 KEYWORD_PATH = os.path.join(cwd, 'inputs', 'keywords_test.csv')
 DRIVER_PATH = os.path.join(cwd, 'venv', 'chromedriver')
 SPQM_OUTPUT_PATH = os.path.join(cwd, 'outputs', 'spqm_output.html')
-SPQM_URL = 'http://www.parliament.scot/parliamentarybusiness/28877.aspx?SearchType=Advance&Keyword=alcohol&ExactPhrase=True&DateTo=23/07/2018%2023:59:59&SortBy=DateSubmitted&Answers=All&SearchFor=All&ResultsPerPage=100'
+SPQM_URL = 'http://www.parliament.scot/parliamentarybusiness/28877.aspx?SearchType=Advance&DateChoice=2&SortBy=DateSubmitted&Answers=OnlyQuestionAwaitingAnswer&SearchFor=WrittenQuestions&ResultsPerPage=100'#'http://www.parliament.scot/parliamentarybusiness/28877.aspx?SearchType=Advance&DateChoice=1&SortBy=DateSubmitted&Answers=OnlyQuestionAwaitingAnswer&SearchFor=WrittenQuestions&ResultsPerPage=100'#'http://www.parliament.scot/parliamentarybusiness/28877.aspx?SearchType=Advance&Keyword=alcohol&ExactPhrase=True&DateTo=23/07/2018%2023:59:59&SortBy=DateSubmitted&Answers=All&SearchFor=All&ResultsPerPage=100'
+SHOULD_HIGHLIGHT = True
 
 keyword_reader = KeywordReader(KEYWORD_PATH)
 keywords = keyword_reader.get_keywords()
@@ -16,4 +18,4 @@ spqm_scraper = SPQMScraper(SPQM_URL, DRIVER_PATH)
 entries = spqm_scraper.scrape()
 
 report_builder = ReportBuilder(SPQM_OUTPUT_PATH)
-report_builder.generate(entries, keywords)
+report_builder.generate(entries, keywords, SHOULD_HIGHLIGHT)
